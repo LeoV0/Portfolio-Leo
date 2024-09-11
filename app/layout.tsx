939de0fa2d536da3b75a8572e25d53311f136d21
Léo <leo.cigale@gmail.com>
2024-09-11 14:35:29 +0200

    new layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,18 @@
 import type { Metadata } from "next";
-import { cn } from "@/lib/utils";
-// import localFont from "next/font/local";
+import localFont from "next/font/local";
 import "./globals.css";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-import { Anek_Telugu } from "next/font/google";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+// Charger les polices locales
+const geistSans = localFont({
+  src: "/fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
 
-const AnkedTelugu = Anek_Telugu({ subsets: ["latin"],
-  variable: "--font-caption",
+const geistMono = localFont({
+  src: "/fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -28,12 +22,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn(GeistSans.variable,GeistMono.variable, AnkedTelugu.variable, "font-sans h-full bg-background text-foreground")}>
+      <body className={`${geistSans.variable} ${geistMono.variable} font-sans h-full bg-background text-foreground`}>
         {children}
       </body>
     </html>
